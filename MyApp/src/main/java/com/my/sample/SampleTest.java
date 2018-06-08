61854979fa28f500f3c7b577c57536421606eb40
deepesh <deepsh2611@gmail.com>
2018-06-08 18:09:17 +0530

    result

--- MyApp/src/main/java/com/my/sample/SampleTest.java
+++ MyApp/src/main/java/com/my/sample/SampleTest.java
@@ -4,7 +4,6 @@ public class SampleTest {
 
 	public static void main(String[] args) {
 		System.out.println(cal(5, 6));
-		System.out.println("cal");
 	}
 	public static int cal(int n,int m){
 		int calculation=m*n;
