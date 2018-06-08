b04e3084159cde09722168e0b8b733c7798e6879
deepesh <deepsh2611@gmail.com>
2018-06-08 16:59:55 +0530

    first commit!!

---
+++ MyApp/src/main/java/com/my/sample/SampleTest.java
@@ -0,0 +1,13 @@
+package com.my.sample;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		cal(5, 6);
+	}
+	public static int cal(int n,int m){
+		int calculation=m*n;
+		return calculation;
+	}
+
+}
