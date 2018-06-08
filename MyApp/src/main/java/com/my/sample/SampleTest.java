079fc139b02417b7771f28b4470d4a085685ff3a
deepesh <deepesh.tiwari2611@gmail.com>
2018-06-08 17:15:03 +0530

    dispaly cal

--- MyApp/src/main/java/com/my/sample/SampleTest.java
+++ MyApp/src/main/java/com/my/sample/SampleTest.java
@@ -4,6 +4,7 @@ public class SampleTest {
 
 	public static void main(String[] args) {
 		System.out.println(cal(5, 6));
+		System.out.println("cal");
 	}
 	public static int cal(int n,int m){
 		int calculation=m*n;
